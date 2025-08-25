65e3d89b1263206fe5eccd6831f3b8445113d424
Aida Halitaj <halitajaida@gmail.com>
2025-08-25 23:26:17 +0000

    added shell scripts to tune parameters

--- grid_search.sh
+++ grid_search.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dvc exp run -S 'model.random_forest.n_estimators=range(50, 200, 50)' \
-            -S 'model.random_forest.max_depth=range(5, 20, 5)' --queue
+dvc exp run -S 'models.random_forest.n_estimators=range(50, 200, 50)' \
+            -S 'models.random_forest.max_depth=range(5, 20, 5)' --queue
