d7e1e1af87fd1feeb412dd23d3ddd602085684d1
Aida Halitaj <halitajaida@gmail.com>
2025-08-26 23:04:42 +0000

    tune hyperparameters for all algorithms

--- grid_search.sh
+++ grid_search.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-dvc exp run -S 'models.random_forest.n_estimators=range(50, 200, 50)' \
-            -S 'models.random_forest.max_depth=range(5, 20, 5)' --queue
+dvc exp run \
+    -S 'models.random_forest.n_estimators=range(50, 200, 50)' \
+    -S 'models.random_forest.max_depth=range(5, 20, 5)' --queue
--- queue_all_experiments.sh
+++ queue_all_experiments.sh
@@ -3,32 +3,32 @@
 # 🟦 Random Forest hyperparameter sweep
 dvc exp run \
   -S model.name=random_forest \
-  -S models.random_forest.n_estimators=range(50, 201, 50) \
-  -S models.random_forest.max_depth=range(5, 21, 5) \
+  -S 'models.random_forest.n_estimators=range(50, 201, 50)' \
+  -S 'models.random_forest.max_depth=range(5, 21, 5)' \
   --queue
 
 # 🟧 Logistic Regression sweep
 dvc exp run \
   -S model.name=logistic_regression \
-  -S models.logistic_regression.C=range(0.1, 1.1, 0.3) \
+  -S 'models.logistic_regression.C=range(0.1, 1.1, 0.3)' \
   --queue
 
 # 🟩 Decision Tree sweep
 dvc exp run \
   -S model.name=decision_tree \
-  -S models.decision_tree.max_depth=range(4, 13, 4) \
+  -S 'models.decision_tree.max_depth=range(4, 13, 4)' \
   --queue
 
 # 🟨 SVM sweep
 dvc exp run \
   -S model.name=svm \
-  -S models.svm.C=range(0.1, 1.1, 0.3) \
+  -S 'models.svm.C=range(0.1, 1.1, 0.3)' \
   --queue
 
 # 🟥 XGBoost sweep
 dvc exp run \
   -S model.name=xgboost \
-  -S models.xgboost.n_estimators=range(50, 151, 50) \
-  -S models.xgboost.max_depth=range(3, 10, 3) \
-  -S models.xgboost.learning_rate=range(0.01, 0.11, 0.03) \
+  -S 'models.xgboost.n_estimators=range(50, 151, 50)' \
+  -S 'models.xgboost.max_depth=range(3, 10, 3)' \
+  -S 'models.xgboost.learning_rate=range(0.01, 0.11, 0.03)' \
   --queue
